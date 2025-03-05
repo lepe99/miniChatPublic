0b5285e5ff0dfeba59fee09968f6a49629c55c37
lepe99 <kr_@kakao.com>
2025-03-05 11:11:24 +0900

    프로젝트 생성

---
+++ src/main/java/org/boot/minichatproject/MiniChatProjectApplication.java
@@ -0,0 +1,13 @@
+package org.boot.minichatproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniChatProjectApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MiniChatProjectApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/org/boot/minichatproject/MiniChatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boot.minichatproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniChatProjectApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
