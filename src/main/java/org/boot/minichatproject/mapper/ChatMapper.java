66c13f323547e63111430276fe484d7f1eefa9c2
lepe99 <kr_@kakao.com>
2025-03-06 15:04:45 +0900

    db 세팅 완료

--- src/main/java/org/boot/minichatproject/mapper/ChatMapper.java
+++ src/main/java/org/boot/minichatproject/mapper/ChatMapper.java
@@ -1,4 +1,23 @@
 package org.boot.minichatproject.mapper;
 
-public class ChatMapper {
+import org.apache.ibatis.annotations.Mapper;
+import org.boot.minichatproject.dto.ChatDto;
+
+import java.util.List;
+
+@Mapper
+public interface ChatMapper {
+    
+    // 채팅 목록 조회 (최근 100개)
+    List<ChatDto> selectChatList();
+    
+    // 채팅 등록
+    void insertChat(ChatDto chatDto);
+    
+    // 채팅 갯수 조회
+    int selectChatCount();
+    
+    // 채팅 삭제 (제일 오래된 1개)
+    void deleteFirstChat();
+    
 }
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,40 +1,35 @@
 // chat.jsp 페이지로 옮길 스크립트
 
 
+$(function () {
 
+    sendButton.click(function () {
+        let message = {
+            nickname: "${sessionScope.nickname}",
+            profileImage: "${sessionScope.profileImage}",
+            content: $("#messageInput").val()
+        };
+        // 메세지 전송
+        socket.send(JSON.stringify(message));
 
-// 메세지 출력
-function displayMessage(message) {
-    console.log("메세지 출력");
-    let timestamp = new Date().toLocaleTimeString([],{hour:'2-digit',minute:'2-digit'});
-
-    let messageHtml = "";
-    if (message.nickname === "${sessionScope.nickname}") {
-        //내 메세지(오른쪽 하단 정렬, 프로필 닉네임 표시안하고 타임스탬프 왼쪽)
-        messageHtml = `
-                    <div class="mymessage">
-                        <span class="timestamp">${timestamp}</span>
-                        <div class="messagecontent">${message.content}</div>
-                    </div>
-                    `;
-    } else {
-        //상대방메세지 (왼쪽 하단 정렬, 프로필+닉네임 표시 , 타임스탬프 오른쪽)
-        messageHtml = `
-                    <div class="othermessage">
-                        <div class="prifilecontainer">
-                            <img src="${message.profileImage}" class="profileImg">
-                            <span class="nickname">${message.nickname}</span>
-                        </div>
-                        <div class="messagecontainer">
-                            <div class="messagecontent">${message.content}</div>
-                            <span class="timestamp">${timestamp}</span>
-                        </div>
-                    </div>
-                    `;
-    }
-
-    // console.log("추가할 HTML:", messageHtml); // ✅ HTML 코드 확인
-    chatBox.append(messageHtml);
-    // console.log("현재 chatBox 내용:", chatBox.html()); // ✅ chatBox에 추가되었는지 확인
-    chatBox.scrollTop(chatBox.prop("scrollHeight")); // 스크롤 아래로 이동
-}
+        // db에 저장
+        $.ajax({
+            url: "insert",
+            type: "post",
+            data: {content: $("#messageInput").val()},
+            success: (response) => {
+                if (response === "success") {
+                    console.log("메세지 저장 성공");
+                } else {
+                    console.log("메세지 저장 실패");
+                }
+            },
+            error : (xhr, status, error) => {
+                console.error("status: " + xhr.status);
+                console.error("error: " + error);
+            }
+        })
+        // 입력창 초기화
+        $("#messageInput").val("");
+    });
+});
