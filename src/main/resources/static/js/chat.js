71a65d8d52824c5660f630669956fa1d327cb751
lepe99 <kr_@kakao.com>
2025-03-08 01:52:46 +0900

    이미지 미리보기 관련 수정

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -73,7 +73,8 @@ $(function () {
                 imagePopover.show(); //popover 표시
             };
             reader.readAsDataURL(file);
-        } else { //파일 선택 취소시 popover 제거
+        } else if (file === undefined) {
+            //파일 선택 취소시 popover 제거
             if (imagePopover) {
                 imagePopover.dispose();
                 imagePopover = null;
