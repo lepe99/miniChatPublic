a99a45ef9596eb78b9454686eeb790f2ecab1710
lepe99 <kr_@kakao.com>
2025-03-10 01:03:48 +0900

    웹 푸시 기능 구현 테스트

--- src/main/java/org/boot/minichatproject/config/FirebaseConfig.java
+++ src/main/java/org/boot/minichatproject/config/FirebaseConfig.java
@@ -6,23 +6,27 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
     
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount =
-                // 서비스 계정 키 파일 경로
-                new FileInputStream("src/main/resources/firebase/minichat-d6f16-firebase-adminsdk-fbsvc-3630a1908d.json");
+        // ClassPathResource를 사용하여 클래스패스에서 리소스 로드
+        ClassPathResource serviceAccount = new ClassPathResource("firebase/minichat-d6f16-firebase-adminsdk-fbsvc-3630a1908d.json");
         
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-        
-        return FirebaseApp.initializeApp(options);
+        // InputStream으로 열기
+        try (InputStream inputStream = serviceAccount.getInputStream()) {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(inputStream))
+                    .build();
+            
+            return FirebaseApp.initializeApp(options);
+        }
     }
 }
